04924f8f8f1b677faa9febeb7b27cde0dbe07196
cheshahith <cheshahith1106@gmail.com>
2025-01-31 15:20:26 +0530

    complete milestone8

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from './pages/LoginPage';  // Import directly
+import SignUpPage from './pages/SignUpPage';
+import Home from './pages/Home';
+import "./App.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
